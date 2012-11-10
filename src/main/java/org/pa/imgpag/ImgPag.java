29262a57d40841df3fe76d4726341c55fb79c2b4
palador <palador@projektangriff.de>
2012-11-10 20:36:13 +0100

    Added filewalker and simple html generation

--- src/main/java/org/pa/imgpag/ImgPag.java
+++ src/main/java/org/pa/imgpag/ImgPag.java
@@ -1,12 +1,19 @@
 package org.pa.imgpag;
 
 import static java.lang.System.err;
-import static java.lang.System.out;
+import static java.nio.file.Files.isDirectory;
+import static java.nio.file.Files.walkFileTree;
+import static java.util.Collections.sort;
 
-import java.io.PrintWriter;
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
 import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.Comparator;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.HelpFormatter;
@@ -37,8 +44,9 @@ public class ImgPag {
 			CommandLine cli = cliParser.parse(cliOptions, args);
 			printHelp = cli.hasOption("h");
 			imgDirPath = cli.getOptionValue("d", ".");
-			if (imgDirPath == null) {
-				imgDir = Paths.get(imgDirPath);
+			imgDir = Paths.get(imgDirPath);
+			if (!isDirectory(imgDir)) {
+				throw new InvalidPathException(imgDirPath, "no a directory");
 			}
 		} catch (InvalidPathException e) {
 			err.println("Invalid path: " + imgDirPath);
@@ -50,16 +58,75 @@ public class ImgPag {
 
 		if (printHelp) {
 			HelpFormatter helpFormatter = new HelpFormatter();
-			helpFormatter
-					.printHelp(
-							new PrintWriter(out),
-							80,
-							"usage: imgpag [OPTIONS]",
-							"ImgPag is a tiny tool to create simple gallery pages of images",
-							cliOptions, 4, 4, "");
+			helpFormatter.printHelp("imgpag [OPTIONS]", cliOptions);
 		}
 
 		if (areArgsValid) {
+			try {
+				final ArrayList<Path> imgFiles = new ArrayList<Path>();
+				walkFileTree(imgDir, new FileVisitor<Path>() {
+
+					Path root;
+
+					@Override
+					public FileVisitResult postVisitDirectory(Path dir,
+							IOException exc) throws IOException {
+						return FileVisitResult.CONTINUE;
+					}
+
+					@Override
+					public FileVisitResult preVisitDirectory(Path dir,
+							BasicFileAttributes attrs) throws IOException {
+						if (root == null || root.equals(dir)) {
+							root = dir;
+							return FileVisitResult.CONTINUE;
+						} else {
+							return FileVisitResult.SKIP_SUBTREE;
+						}
+					}
+
+					@Override
+					public FileVisitResult visitFile(Path file,
+							BasicFileAttributes attrs) throws IOException {
+						if (file.toString().toLowerCase().endsWith(".jpg")) {
+							imgFiles.add(file);
+						}
+						return FileVisitResult.CONTINUE;
+					}
+
+					@Override
+					public FileVisitResult visitFileFailed(Path file,
+							IOException exc) throws IOException {
+						throw exc;
+					}
+				});
+
+				sort(imgFiles, new Comparator<Path>() {
+
+					@Override
+					public int compare(Path arg0, Path arg1) {
+						return arg0.getFileName().toString()
+								.compareTo(arg1.getFileName().toString());
+					}
+				});
+
+				StringBuilder indexHtml = new StringBuilder();
+				indexHtml
+						.append("<html>\n    <head>\n        <title>gallery</title>\n    </head>\n    <body>\n");
+				for (Path file : imgFiles) {
+					String name = file.getFileName().toString();
+					String nameWOutExt = name.substring(0,
+							name.lastIndexOf('.'));
+					indexHtml.append("        <a href=\"").append(name)
+							.append("\">")
+							.append(nameWOutExt).append("</a><br>\n");
+				}
+				indexHtml.append("    </body>\n</html>");
+				System.out.println(indexHtml);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	}
 
